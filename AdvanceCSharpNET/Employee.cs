2caa7c0739f0433181f7c46e3395a6999c790ad9
buihuudong19 <buihuudong19@gmail.com>
2022-10-24 19:25:03 +0700

    Add project files.

---
+++ AdvanceCSharpNET/Employee.cs
@@ -0,0 +1,67 @@
+﻿namespace AdvanceCSharpNET.Entities;//namespace, class, interface, delegate
+public class Employee
+{
+    //fields/data
+    private int _id;
+    private static int _lastId = default;//c#7.1 
+    public static int NextId => ++_lastId;//++a|a++ <=> a = a+1; (unary)
+    private string? _name;
+    private DateOnly _dob;
+    private string _address;
+    //properties
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    public string? Name { get => _name; set => _name = value; }//Expression-body
+    public DateOnly Dob{ get => _dob; set => _dob = value; }
+    public string Address { get => _address; set => _address = value; }
+    //toi uu
+    public int WorkHour { get; set; } = 0;
+    public double SalaryRate { get; set; } = 1.0d;
+
+    //Mảng 1 chiều cho phép chứa nhiều employees
+    public static Employee[] Employees { get;}//read-only => mang tinh
+    private int _size = 0;
+
+    //constructors
+    //static constructor
+    static Employee()
+    {
+        Employees = new Employee[10];
+       
+    }
+    public Employee() { }//explicit
+    
+    public Employee(int id, string? name, DateOnly dob, string? address, int workHour, double salaryRate)
+    {
+        Id = id;
+        Name = name;
+        Dob = dob;
+        Address = address;
+        WorkHour = workHour;
+        SalaryRate = salaryRate;
+    }
+   
+    //Top Level
+    public override string? ToString() => Print();
+
+    //methods (functions/behaviors)
+    private string Print() => $"Id: {Id}, Name: {this._name}, Dob: {Dob}, Address: {Address}, Work Hour: {WorkHour}, Salary rate: {SalaryRate}";
+
+    //method save 1 employee neu nhung emp do chua co, neu co thi update
+    public void Save()
+    {
+        //Lam the nào để lưu 1 emp vào trong mảng 1 chiều
+    }
+
+    //display all employee (procedure)
+    public void Display()
+    {
+        for(int i = 0; i < this._size; i++)
+        {
+            Console.WriteLine(Employees[i]);
+        }
+    }
+}
---
+++ AdvanceCSharpNET/Program.cs
@@ -0,0 +1,27 @@
+﻿using AdvanceCSharpNET.Entities;
+namespace Manager.Employees;
+public class Program
+{
+    public static void Main()
+    {
+        //Tao ra 1 doi tuong Employee 
+        Employee e = new Employee();
+        e.Id = 1;
+        e.Name = "Bui Huu Dong";
+        e.Address = "Quan 10, TPHCM";
+        e.Dob=new DateOnly(1983,03,19);//Overloading (nap chong/qua tai)
+        e.WorkHour = 25;
+        e.SalaryRate = 250_000; //number literal
+
+        Employee e2 = new Employee(2,"Tran Hau Binh",new DateOnly(2002,10,10), "Quan 2, TPHCM", 23, 300_000);
+
+        Employee e3 = new Employee();
+        //var/dynamic
+
+        int[] array = null;
+        Console.WriteLine(array?.Length);
+
+        Console.ReadLine();
+        
+    }
+}
